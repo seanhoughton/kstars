d2e831102fc6d5f8d7bbbc5f0de410a92eb0e015
Sean Houghton <sean.houghton@gmail.com>
2016-11-21 21:04:32 -0800

    Cache the Cellar directory

--- travis-ci/deps-osx.sh
+++ travis-ci/deps-osx.sh
@@ -9,13 +9,13 @@ brew tap polakovic/astronomy
 
 CELLAR_URL=https://s3-us-west-1.amazonaws.com/kstars-cache.cerebiggum.com/Cellar.tgz
 
-echo "Downloading Cellar..."
-mkdir -p /usr/local
-curl -o /usr/local/Cellar.tgz ${CELLAR_URL}
-cd /usr/local
-
-echo "Extracting Cellar..."
-tar xfz Cellar.tgz
-
-echo "Linking Cellar..."
-brew link
+if [ ! -d /usr/local/Cellar/qt ]; then
+    echo "Downloading Cellar..."
+    mkdir -p /usr/local
+    curl -o /usr/local/Cellar.tgz ${CELLAR_URL}
+    cd /usr/local
+    echo "Extracting Cellar..."
+    tar xfz Cellar.tgz
+    echo "Linking Cellar..."
+    brew link
+fi
